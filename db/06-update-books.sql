a6763817163a93cae2ae7df9a5071f6c54c77792
Harrison Royds <harrisonroyds@Harrisons-MacBook-Pro.local>
2022-11-03 14:37:07 +0000

    6 Done :)

--- db/06-update-books.sql
+++ db/06-update-books.sql
@@ -1,5 +1,8 @@
 \c my_bookshop
 
 \echo '\n These books have been put on sale, due to excess stock:\n'
-
-
+SELECT * FROM book_shelf;
+UPDATE book_shelf
+SET price_in_pence = price_in_pence * 0.9
+WHERE quantity_in_stock > 10;
+SELECT * FROM book_shelf ORDER BY book_id ASC;
