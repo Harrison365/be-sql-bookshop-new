2d58676e3fc27d417a83e1ee224e7b51c6bd0178
Harrison Royds <harrisonroyds@Harrisons-MacBook-Pro.local>
2022-11-03 15:56:00 +0000

    finished task 11

--- db/10-update-books-author.sql
+++ db/10-update-books-author.sql
@@ -1,33 +1,35 @@
 \c my_bookshop
 
-UPDATE books
-SET author_id = 3
+UPDATE book_shelf
+SET author = 3
 WHERE book_id = 1;
 
-UPDATE books
-SET author_id = 2
+UPDATE book_shelf
+SET author = 2
 WHERE book_id = 2;
 
-UPDATE books
-SET author_id = 7
+UPDATE book_shelf
+SET author = 7
 WHERE book_id = 3;
 
-UPDATE books
-SET author_id = 14
+UPDATE book_shelf
+SET author = 14
 WHERE book_id = 4;
 
-UPDATE books
-SET author_id = 13
+UPDATE book_shelf
+SET author = 13
 WHERE book_id = 5;
 
-UPDATE books
-SET author_id = 15
+UPDATE book_shelf
+SET author = 15
 WHERE book_id = 6;
 
-UPDATE books
-SET author_id = 12
+UPDATE book_shelf
+SET author = 12
 WHERE book_id = 7;
 
-UPDATE books
-SET author_id = 14
+UPDATE book_shelf
+SET author = 14
 WHERE book_id = 10;
+
+SELECT * FROM book_shelf;
--- db/11-select-books-with-author.sql
+++ db/11-select-books-with-author.sql
@@ -2,7 +2,9 @@
 
 \echo '\n Here is a list of book titles with the corresponding author:\n'
 
-
+SELECT title FROM book_shelf LEFT JOIN authors_table ON authors_table.author_id = book_shelf.author WHERE authors_table.author_name = 'Jane Austen';
 
 \echo '\n Here is a list of authors without an associated book:\n'
+
+SELECT author_name FROM authors_table LEFT JOIN book_shelf ON authors_table.author_id = book_shelf.author WHERE book_shelf.book_id IS NULL
 
