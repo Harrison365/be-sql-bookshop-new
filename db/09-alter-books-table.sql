2aa10571b1f1df9369d9b74ab00271bd3c390747
Harrison Royds <harrisonroyds@Harrisons-MacBook-Pro.local>
2022-11-04 17:04:26 +0000

    all done

--- db/09-alter-books-table.sql
+++ db/09-alter-books-table.sql
@@ -2,5 +2,5 @@
 
 ALTER TABLE book_shelf
 ADD COLUMN author 
-INT REFERENCES authors_table (author_id) ON DELETE CASCADE;
+INT REFERENCES authors_table (author_id) ON DELETE CASCADE; --needed for Q18 part 2 
 SELECT * FROM book_shelf;
