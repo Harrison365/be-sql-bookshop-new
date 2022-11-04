04dac4142303b83f12df614c52d7c0a9ed9cc902
HannahBurks <hannahburks@hotmail.com>
2022-11-04 12:32:27 +0000

    this code is for task 16 and 17

--- db/16-query-books-with-genres.sql
+++ db/16-query-books-with-genres.sql
@@ -1,8 +1,16 @@
 \c my_bookshop
 
 \echo '\n Here are the genres for Emma:\n'
+SELECT * FROM genres
+JOIN genre_book_junction ON genres.genre_id = genre_book_junction.genre_id
+JOIN book_shelf ON book_shelf.book_id = genre_book_junction.book_id
+WHERE book_shelf.title = 'Emma';
 
 
 
 \echo '\n Here are all the dystopian books:\n'
 
+SELECT * FROM book_shelf
+JOIN genre_book_junction ON book_shelf.book_id = genre_book_junction.book_id
+JOIN genres ON genres.genre_id = genre_book_junction.genre_id
+WHERE genres.genre_name = 'dystopian';
--- db/17-aggregate-queries.sql
+++ db/17-aggregate-queries.sql
@@ -2,7 +2,12 @@
 
 \echo '\n Here is the total number of books we have by each author:\n'
 
+SELECT * FROM book_shelf, COUNT (authors_table.author_id)
+LEFT JOIN authors_table ON book_shelf.author = authors_table.author_id;
 
+
+-- SELECT SUM(number_of_legs) AS total_number_of_legs
+-- FROM animals;
 
 \echo '\n Here is the average price for dystopian books:\n'
 
