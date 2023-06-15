b91f45af7150b6e82656afe935bcc0acf4e928cb
Harrison Royds <harrisonroyds@Harrisons-MacBook-Pro.local>
2023-06-15 10:41:29 +0100

    comments on running files added

--- db/01-create-database.sql
+++ db/01-create-database.sql
@@ -4,3 +4,9 @@ DROP DATABASE IF EXISTS my_bookshop;
 CREATE DATABASE my_bookshop;
 --connect to db
 \c my_bookshop
+--to run a psql file psql -f filename (> where-you-want-it-to-go)
+
+--To console.log in termiinal
+--DO $$ BEGIN
+--   RAISE NOTICE 'Your message here';
+--END $$;
--- run-all.sh
+++ run-all.sh
@@ -2,3 +2,5 @@
 for file in "./db"/*.sql; do
     psql -f "${file}" > ${file%.sql}.txt
 done
+
+# run using sh run-all.sh
