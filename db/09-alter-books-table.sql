031bf6663fcca147902d89590d7e1e5227100e2c
Harrison Royds <harrisonroyds@yahoo.co.uk>
2023-11-09 17:11:09 +0000

    comments added for notes

--- db/09-alter-books-table.sql
+++ db/09-alter-books-table.sql
@@ -7,7 +7,7 @@ SELECT * FROM book_shelf;
 
 --can also write this as 
 -- ALTER TABLE book_shelf
--- ADD COLUMN author,
--- ADD FOREIGN KEY (author) REFERENCES authors_table (author_id) 
+-- ADD COLUMN author INT,
+-- ADD FOREIGN KEY (author) REFERENCES authors_table (author_id);
 
 --PSQL assumes that the column name and the foreign key name are the same if it references another table
