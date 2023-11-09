ed745b6de51f59a717cba3581f0f4ecbb8477724
Harrison Royds <harrisonroyds@yahoo.co.uk>
2023-11-09 11:57:15 +0000

    typo fix and drop table if exists added

--- db/02-create-books-table.sql
+++ db/02-create-books-table.sql
@@ -1,4 +1,5 @@
 \c my_bookshop;
+DROP TABLE IF EXISTS book_shelf;
 -- create the books table here
 CREATE TABLE book_shelf(book_id SERIAL PRIMARY KEY, title VARCHAR(100) NOT NULL, price_in_pence INT NOT NULL, quantity_in_stock INT NOT NULL, release_date DATE NOT NULL, is_fiction BOOLEAN NOT NULL);
 
