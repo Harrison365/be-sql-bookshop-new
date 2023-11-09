0a7833782ffe9c2bbf481866ebf44f713646afe4
Harrison Royds <harrisonroyds@yahoo.co.uk>
2023-11-09 16:34:49 +0000

    narrowed down select query to specific columns

--- db/16-query-books-with-genres.sql
+++ db/16-query-books-with-genres.sql
@@ -1,7 +1,7 @@
 \c my_bookshop
 
 \echo '\n Here are the genres for Emma:\n'
-SELECT * FROM genres
+SELECT genre_name FROM genres
 JOIN genre_book_junction ON genres.genre_id = genre_book_junction.genre_id
 JOIN book_shelf ON book_shelf.book_id = genre_book_junction.book_id
 WHERE book_shelf.title = 'Emma';
@@ -10,7 +10,7 @@ WHERE book_shelf.title = 'Emma';
 
 \echo '\n Here are all the dystopian books:\n'
 
-SELECT * FROM book_shelf
+SELECT title FROM book_shelf
 JOIN genre_book_junction ON book_shelf.book_id = genre_book_junction.book_id
 JOIN genres ON genres.genre_id = genre_book_junction.genre_id
 WHERE genres.genre_name = 'dystopian';
