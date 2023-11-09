b42f97f365eeed0583dbd2a94107835dd28ca13e
Harrison Royds <harrisonroyds@yahoo.co.uk>
2023-11-09 15:53:00 +0000

    comments added for notes

--- db/09-alter-books-table.sql
+++ db/09-alter-books-table.sql
@@ -2,5 +2,12 @@
 
 ALTER TABLE book_shelf
 ADD COLUMN author 
-INT REFERENCES authors_table (author_id) ON DELETE CASCADE; --needed for Q18 part 2 
+INT REFERENCES authors_table (author_id) ON DELETE CASCADE; --ON DELETE added in Q18 part 2 
 SELECT * FROM book_shelf;
+
+--can also write this as 
+-- ALTER TABLE book_shelf
+-- ADD COLUMN author,
+-- ADD FOREIGN KEY (author) REFERENCES authors_table (author_id) 
+
+--PSQL assumes that the column name and the foreign key name are the same if it references another table
