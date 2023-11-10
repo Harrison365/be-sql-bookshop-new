4b9db3b055bd8949766b64191f86769f7e1fc171
Harrison Royds <harrisonroyds@yahoo.co.uk>
2023-11-10 16:15:12 +0000

    spaces removed

--- db/16-query-books-with-genres.sql
+++ db/16-query-books-with-genres.sql
@@ -6,8 +6,6 @@ JOIN genre_book_junction ON genres.genre_id = genre_book_junction.genre_id
 JOIN book_shelf ON book_shelf.book_id = genre_book_junction.book_id
 WHERE book_shelf.title = 'Emma';
 
-
-
 \echo '\n Here are all the dystopian books:\n'
 
 SELECT title FROM book_shelf
